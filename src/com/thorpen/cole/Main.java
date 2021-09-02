3c958dcafbecaeabba7220308bcb5df3a172bffc
Cole Thorpen <thorpencole@gmail.com>
2021-09-02 11:36:49 -0700

    initial

---
+++ src/com/thorpen/cole/Main.java
@@ -0,0 +1,9 @@
+package com.thorpen.cole;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello CPSC 312 !!");
+    }
+}
